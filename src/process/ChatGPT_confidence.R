fa6276f7cc9152b1d7835e43df2d9b1ffa6d1e53
RyanWen229 <142018395+RyanWen229@users.noreply.github.com>
2025-01-05 06:42:05 -0500

    Update ChatGPT_confidence.R

--- src/process/ChatGPT_confidence.R
+++ src/process/ChatGPT_confidence.R
@@ -1,16 +1,4 @@
-library(stringr)
-library(dplyr)
-library(tidyr)
-library(jsonlite)
-library(pbapply)
-library(future)
-library(httr2)
-
-load("rumor_facts_with_na.RData")
-
-final_df = distinct(final_df)
-base_url = "https://api.openai.com/v1/chat/completions"
-api_key = Sys.getenv("OPENAI_API_KEY")
+source('__init__.R')
 
 process_row <- function(row) {
   # Check for missing values
